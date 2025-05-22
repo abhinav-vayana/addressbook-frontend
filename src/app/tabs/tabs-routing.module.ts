787736d9105a237cf3986def54a4a0bbc5c0e507
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-22 16:27:16 +0530

    fixed routing bug

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -5,7 +5,7 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/tabs/contacts',
+    redirectTo: 'tabs/contacts',
     pathMatch: 'full',
   },
   {
